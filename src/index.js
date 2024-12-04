e7d0100351a2dfe60f0d1dad5dff4b609b95c430
Sugishin073 <buckesbeats@gmail.com>
2024-12-04 16:29:03 +0900

    リアクト基礎

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';を以下に変更
+import Routing from "./routing";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>
 );
 
